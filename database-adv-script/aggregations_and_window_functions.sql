7e43152e0b9e29616c67d6e56b583f44bacf17ec
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-18 12:22:12 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,3 +9,11 @@ LEFT JOIN
     Booking b ON p.property_id = b.property_id
 GROUP BY 
     p.property_id, p.name;
+
+SELECT 
+    property_id,
+    property_name,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank
+FROM 
+    property_booking_counts;
