ca65523e41947ad943468dc99d47508b83eb2a47
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-18 15:41:22 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -6,6 +6,8 @@ CREATE INDEX idx_booking_user_id ON Booking(user_id);
 -- 2. Index on Booking.property_id (used in JOINs and WHERE clauses)
 CREATE INDEX idx_booking_property_id ON Booking(property_id);
 
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+
 -- 3. Index on Property.name (useful for search or ORDER BY operations)
 CREATE INDEX idx_property_name ON Property(name);
 
@@ -13,4 +15,13 @@ CREATE INDEX idx_property_name ON Property(name);
 CREATE INDEX idx_user_name ON User(name);
 
 -- 5. Composite Index on Booking (user_id, property_id) for multi-condition JOINs
-CREATE INDEX idx_booking_user_property ON Booking(user_id, property_id);u
+CREATE INDEX idx_booking_user_property ON Booking(user_id, property_id);
+
+--After index applying
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = 2;
+
+
+
