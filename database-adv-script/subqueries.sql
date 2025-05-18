750a94927e8b0dd632968e2850602a7ee3a7ff1d
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-18 11:18:17 +0300

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,32 @@
+SELECT 
+    p.property_id,
+    p.name AS property_name
+FROM 
+    Property p
+WHERE 
+    p.property_id IN (
+        SELECT 
+            r.property_id
+        FROM 
+            Review r
+        GROUP BY 
+            r.property_id
+        HAVING 
+            AVG(r.rating) > 4.0
+    );
+
+SELECT 
+    u.user_id,
+    u.name AS user_name,
+    u.email
+FROM 
+    User u
+WHERE (
+    SELECT 
+        COUNT(*) 
+    FROM 
+        Booking b 
+    WHERE 
+        b.user_id = u.user_id
+) > 3;
+
