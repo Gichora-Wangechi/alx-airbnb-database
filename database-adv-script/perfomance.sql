2566a98ffb4bbc906ca7f2a9861d94bddeb463cf
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-18 15:35:06 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,18 +1,6 @@
 --Initial query (complex join)
 -- Initial query: Retrieve all bookings with user, property, and payment details
-SELECT 
-    b.id AS booking_id,
-    b.start_date,
-    b.end_date,
-    u.id AS user_id,
-    u.name AS user_name,
-    u.email,
-    p.id AS property_id,
-    p.name AS property_name,
-    p.location,
-    pay.id AS payment_id,
-    pay.amount,
-    pay.payment_date
+SELECT *
 FROM Booking b
 JOIN User u ON b.user_id = u.id
 JOIN Property p ON b.property_id = p.id
