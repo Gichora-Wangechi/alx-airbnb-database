accbd83f5b24dd7b3112ac14106159507be144cc
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-18 11:04:15 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,76 @@
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM 
+    Booking b
+INNER JOIN 
+    User u ON b.user_id = u.user_id;
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.created_at AS review_date
+FROM 
+    Property p
+LEFT JOIN 
+    Review r ON p.property_id = r.property_id;
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price
+FROM 
+    User u
+FULL OUTER JOIN 
+    Booking b ON u.user_id = b.user_id;
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price
+FROM 
+    User u
+LEFT JOIN 
+    Booking b ON u.user_id = b.user_id
+
+UNION
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price
+FROM 
+    User u
+RIGHT JOIN 
+    Booking b ON u.user_id = b.user_id;
+
+
